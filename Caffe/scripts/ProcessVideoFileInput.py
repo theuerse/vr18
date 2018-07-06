be717fcef795993348d5fb70136f32db40b9694c
Your Name <you@example.com>
2018-07-06 14:32:27 +0200

    extended script with concept-detection of shots

--- Caffe/scripts/ProcessVideoFileInput.py
+++ Caffe/scripts/ProcessVideoFileInput.py
@@ -1,17 +1,49 @@
 import os
 import sys
 import time
+import sqlite3
 import multiprocessing as mp
+from subprocess import check_output
 
+# shot detection
 SHOT_DIR = os.path.abspath("shots")
 SHOT_DETECTOR = "/home/theuers/CLionProjects/OpenCV/cmake-build-debug/ShotDetect"
-THRESHOLD = 0.5
+THRESHOLD = 0.7
+
+# concept identification
+CAFFE_DIR = "/home/theuers/caffe/"
+CONCEPT_DETECTOR = os.path.join(CAFFE_DIR, "./examples/cpp_classification/classification.bin")
+MODEL = "bvlc_reference_caffenet"
+DEPLOY_PROTO = os.path.join(CAFFE_DIR, "models/"+ MODEL +"/deploy.prototxt")
+CAFFE_MODEL = os.path.join(CAFFE_DIR, "models/" + MODEL + "/bvlc_reference_caffenet.caffemodel")
+BINARY_PROTO = os.path.join(CAFFE_DIR, "data/ilsvrc12/imagenet_mean.binaryproto")
+SYNSET = os.path.join(CAFFE_DIR, "data/ilsvrc12/synset_words.txt")
+
 
 def detectShots(filepath):
     start_time = time.time()
     print("SHOTDETECT: " + os.path.basename(filepath))
     os.system(" ".join([SHOT_DETECTOR, str(THRESHOLD), filepath, SHOT_DIR]))
     print("END OF SHOTDETECT: " + os.path.basename(filepath) + " " + str(time.time() - start_time) + " seconds")
+
+
+def classifyImage(filepath):
+    start_time = time.time()
+    print("INDEX: " + os.path.basename(filepath))
+
+    output = check_output([CONCEPT_DETECTOR, DEPLOY_PROTO, CAFFE_MODEL, BINARY_PROTO, SYNSET, filepath]).decode("utf-8")
+    concept = output.split("\n")[1].split('"')[1].split()[0]
+
+    conn = sqlite3.connect('./vr.db')
+    c = conn.cursor()
+    c.execute('insert or replace into images values (?,?)', [os.path.basename(filepath), concept])
+    conn.commit()
+    conn.close()
+
+    print("END OF INDEX: " + os.path.basename(filepath) + " " + str(time.time() - start_time) + " seconds")
+
+
+
 
 
 if len(sys.argv) != 2:
@@ -23,6 +55,8 @@ input_dir = sys.argv[1]
 # perform setup
 os.system("sh setup.sh")
 
+
+# shot detection
 fileList = []
 for file in os.listdir(input_dir):
     if(file.endswith(".mp4")):
@@ -30,6 +64,15 @@ for file in os.listdir(input_dir):
 
 pool = mp.Pool(processes=4)
 results = pool.map(detectShots, fileList)
+
+
+
+# image indexing
+fileList = []
+for file in os.listdir(SHOT_DIR):
+    fileList.append(os.path.join(SHOT_DIR, file))
+results = pool.map(classifyImage, fileList)
+
 
 
 
