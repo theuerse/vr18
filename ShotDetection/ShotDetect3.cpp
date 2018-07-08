5188f8501399bc5f77c1ea6db5458afc379d01d0
Your Name <you@example.com>
2018-07-08 10:17:44 +0200

    ECR - attempt

--- ShotDetection/ShotDetect3.cpp
+++ ShotDetection/ShotDetect3.cpp
@@ -37,52 +37,16 @@ inline string getBasename(string path){
     return path;
 }
 
-inline vector<float> calcColorHist(Mat &img){
-    int channels = img.channels();
-    int nRows = img.rows;
-    int nCols = img.cols * channels;
 
-    if(img.isContinuous()) // matrix without gaps?
-        nCols *= nRows; // number of bytes (including all channels)
 
-    vector<float> hist(256);
-    // initialize with zeros
-    for(int i = 0; i < 256; ++i)
-        hist[i] = 0.0;
-
-    uchar b,g,r;
-    uchar *p = img.ptr<uchar>(0);
-    for(int j = 0, q = 0; j < nCols; ++q){
-        b = (p[j++] & 0xC0); // two msb
-        g = (p[j++] & 0xE0) >> 2; // three msb
-        r = (p[j++] & 0xE0) >> 5;
-        hist[b|g|r]++;
-    }
-    /*for(int i = 0; i < hist.size(); i++){
-        cout << hist[i] << " ";
-    }
-    cout << endl;*/
-
-    // normalization of results
-    float sum = nCols / channels;
-    for(int i = 0; i < hist.size(); i++)
-        hist[i] = hist[i] / sum;
-
-    /*for(int i = 0; i < hist.size(); i++){
-        cout << hist[i] << " ";
-    }
-    cout << endl;*/
-    return hist;
-}
-
-inline Mat getHog(Mat &img){
+inline Mat getEdgeMat(Mat &img){
     Mat grad, img_grey, img_clone = img.clone();
     int ddepth = CV_16S;
     int ksize = 1;
     int scale = 1;
     int delta = 0;
 
-    GaussianBlur(img_clone, img_clone, Size(3, 3), 0, 0, BORDER_DEFAULT);
+    //GaussianBlur(img_clone, img_clone, Size(3, 3), 0, 0, BORDER_DEFAULT);
 
     // Convert the image to grayscale
     cvtColor(img_clone, img_grey, COLOR_BGR2GRAY);
@@ -94,77 +58,59 @@ inline Mat getHog(Mat &img){
     convertScaleAbs(grad_x, abs_grad_x);
     convertScaleAbs(grad_y, abs_grad_y);
     addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad);
-    //imshow("bla", grad);
-    //waitKey(0);
-    /*Mat img_clone = img.clone();
-    img_clone.convertTo(img_clone, CV_32F, 1/255.0);
 
-    imshow('sdfs',img_clone);
-    waitKey(0);
-    vector<float> hist(256,0.0);
+    // convert image to monochrome
+    grad = grad > 128;
 
-    // Calculate gradients gx, gy
-    Mat gx, gy;
-    Mat abs_gx, abs_gy;
-    Sobel(img_clone, gx, CV_32F, 1, 0, 1);
-    Sobel(img_clone, gy, CV_32F, 0, 1, 1);
-
-    // converting back to CV_8U
-    convertScaleAbs(gx, abs_gx);
-    convertScaleAbs(gy, abs_gy);
-
-
-    int channels = abs_gx.channels();
-    int nRows = abs_gx.rows;
-    int nCols = abs_gx.cols * channels;
-
-    if(abs_gx.isContinuous()) // matrix without gaps?
-        nCols *= nRows; // number of bytes (including all channels)
-
-
-    uchar x,y;
-    uchar *p = abs_gx.ptr<uchar>(0);
-    uchar *q = abs_gy.ptr<uchar>(0);
-    for(int j = 0; j < nCols; ++j){
-        x = (p[j] & 0xf0);      // four msb
-        y = (q[j] & 0xf0) >> 4; // four msb
-        hist[x|y]++;
-    }
-   */
-    /*
-    uchar x,y;
-    for(int j=0;j<abs_gx.rows;j++)
-    {
-        for (int i=0;i<abs_gx.cols;i++)
-        {
-                //hist[abs_gx.at<uchar>(j,i)]++;
-                //hist[256+abs_gy.at<uchar>(j,i)]++;
-            uchar x1 = abs_gx.at<uchar>(j,i);
-            x = abs_gx.at<uchar>(j,i) & 0xf0;
-            y = (abs_gy.at<uchar>(j,i) & 0xf0) >> 4;
-            hist[x|y]++;
-        }
-    }*/
-
-    // normalize
-    /*float sum = abs_gx.cols * abs_gx.rows;
-    for(int i = 0; i < hist.size(); i++)
-        hist[i] = hist[i] / sum;
-
-    return hist;*/
-   // vector<float> bla;
-   // return bla;
     return grad;
 }
 
-inline float intersection(vector<float> &h1, vector<float> &h2){
-    float intersect = 0;
+inline double getECR(Mat &x, Mat &y){
+    https://de.wikipedia.org/wiki/Edge_Change_Ratio
+    double ecr = 0;
+    // a ... A', b ... B'
+    Mat a = x.clone(), b = y.clone();
 
-    for(int i = 0; i < h1.size(); ++i)
-        intersect += min(h1[i],h2[i]);
+    Mat a_dil, b_dil; // A*, B*
+    dilate(a, a_dil, Mat(), Point(-1, -1), 2, 1, 1);
+    dilate(b, b_dil, Mat(), Point(-1, -1), 2, 1, 1);
 
-    return intersect;
+
+
+    Mat a_inv, b_inv;
+    bitwise_not(a,a_inv);
+    bitwise_not(b,b_inv);
+
+    Mat a_xor, b_xor;
+    // A* und eine invertierte Version des Bildes B' werden XOR-verknüpft
+    bitwise_xor(a_dil,b_inv,a_xor);
+    // ebenso B* und eine invertierte Version des Bildes A'
+    bitwise_xor(b_dil,a_inv,b_xor);
+
+    imshow("a_xor", a_xor);
+    imshow("b_xor", b_xor);
+    waitKey(0);
+
+    //In den so entstehenden Bildern A und B wird jeweils die Anzahl aller gefärbten Pixel gezählt
+    double eIn, eOut, pIn, pOut;
+    eIn = cv::sum(a_xor)[0];
+    eOut = cv::sum(b_xor)[0];
+
+    double a_sum = cv::sum(a)[0];
+    double b_sum = cv::sum(b)[0];
+
+    //  eOut wird durch die Anzahl der Kantenpixel im Kantenbild A' geteilt
+    pOut = eOut/a_sum;
+
+    // eIn wird durch die Anzahl der Kantenpixel im Kantenbild B' geteilt.
+    pIn = eIn/b_sum;
+
+    // Das Ergebnis ist das Kanteneintrittsverhältnis pIn und das Kantenaustrittsverhältnis pOut
+    ecr = max(pIn/a_sum,pOut/b_sum);
+
+    return ecr;
 }
+
 
 
 int main(int argc, char ** argv){
@@ -183,52 +129,27 @@ int main(int argc, char ** argv){
 
     Mat frame;
     VideoCapture cap = VideoCapture(argv[3]);
-    vector<float> lastColorHist, currentColorHist;
     double frameNumber = 0;
-    float colorDelta, hogDelta, similarity = 0;
-    float tdSum = 0;
+    float ecr;
 
-    Mat lastHog, currentHog;
+    Mat lastEdgeMat, currentEdgeMat;
     Mat diff;
 
     cap.read(frame);
-    lastColorHist = calcColorHist(frame);
-    lastHog = getHog(frame);
-    double currentEdgeDiff, lastEdgeDiff = 0;
+    lastEdgeMat = getEdgeMat(frame);
 
     while(cap.read(frame)){
-        //currentColorHist = calcColorHist(frame);
-        currentHog = getHog(frame);
+        currentEdgeMat = getEdgeMat(frame);
 
-        //similarity = intersection(lastColorHist, currentColorHist);
-        //colorDelta = 1 - similarity;
+        ecr = getECR(lastEdgeMat, currentEdgeMat);
 
-        //similarity = intersection(lastHog, currentHog);
-
-        absdiff(lastHog, currentHog, diff);
-        currentEdgeDiff = cv::sum(diff).val[0];
-
-        hogDelta = abs(lastEdgeDiff - currentEdgeDiff)/lastEdgeDiff;
-
-        /*if(colorDelta > td || hogDelta > td){
-            // gradual buildup of change -> detect fading
-            tdSum += colorDelta - td;
-        }else{
-            tdSum = 0;
-        }*/
-
-        //if(colorDelta > th || tdSum > th || hogDelta > th){
-        if(hogDelta > th){
+        if(ecr > th){
             frameNumber = cap.get(CV_CAP_PROP_POS_FRAMES);
             //imshow(to_string(frameNumber),frame);
             cout <<  argv[3]  << ": detected shot border at frame: " << frameNumber << "\n";
             imwrite(outputPath + "@" + to_string((int)frameNumber) + ".png", frame);
-            cout << "lastedgediff: " << lastEdgeDiff << "\t currentEdgeDiff: " << currentEdgeDiff << "\nhogDelta: " << hogDelta << "\n";
-
-            tdSum = 0;
         }
-        //lastColorHist = currentColorHist;
-        lastEdgeDiff = currentEdgeDiff;
+        lastEdgeMat = currentEdgeMat;
 
     }
     return 0;
